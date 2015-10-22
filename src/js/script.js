8fa6a0404f760d8496ed58d147a401313824e097
Сергей Попов <tovarov.piter@gmail.com>
2015-10-22 14:36:02 +0300

    Второе окно и скрипты

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
 $(function() {
 
+  $(".modal-open").on("click", function(){
+    var modal = $(this).attr("data-modal");
+    $(".modal__overlay").addClass("modal__overlay--active");
+    $("." + modal).addClass("modal__overlay--active");
+    return false;
+  })
+
+  $(".modal__close").on("click", function(){
+    $(".modal__overlay").removeClass("modal__overlay--active");
+    $(".modal").removeClass("modal__overlay--active");
+    return false;
+  })
+
 });
